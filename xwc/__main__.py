fb9d3871f915a02525b4cfdcc7b02d41e6436433
XeXerogrammer <xerogrammer@gmail.com>
2024-10-02 12:40:11 +0330

    add line count functionality

--- xwc/__main__.py
+++ xwc/__main__.py
@@ -13,11 +13,49 @@ Written by: xexerogrammer"""
 
     parser.add_argument("-l", "--lines", action="store_true", help="print the newline counts")
     parser.add_argument("-v", "--version", action="store_true", help="output version information and exit")
+    parser.add_argument("FILE", type=str, nargs="*")
 
     args = parser.parse_args()
 
     if args.version:
         print(version_text)
         return 0
+    state = get_state(args)
+    for file in args.FILE:
+        file_details = get_file_details(file)
+        if file_details["lines"] < 0:
+            continue
+        print_report(file, file_details, state)
+    return 0
+
+def get_state(args):
+    # return an integer corrispomding to the state of the program
+    # 1 get lines
+    state = 0;
+    if (args.lines):
+        state ^= 1
+    return state
+
+def get_file_details(fh):
+    file_details = {
+                    "lines": 0,
+                }
+    file = None
+    try:
+        file = open(fh, "r")
+    except Exception:
+        print(fh, ": no such file or directory")
+        file_details["lines"] = -1
+        return file_details
+
+    for line in file:
+        file_details["lines"] += 1;
+
+    return file_details
+
+def print_report(file, file_details, state):
+    if state & 1:
+        print(f'{file_details["lines"]:4}', end=" ")
+    print(file)
 
 main()
